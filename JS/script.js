8e48a49e7e0701ab7451f0992123dc317e029d8f
Asif_hash786 <masif15881@gmail.com>
2025-10-25 01:19:27 +0530

    updated

---
+++ JS/script.js
@@ -0,0 +1,69 @@
+
+// alert("Something is wrong");
+// console.error("This is an error msg");
+// console.warn("This is warning");
+
+// let firstName= prompt("Enter your name");
+// let msg="Welcome " +firstName+" !";
+// alert(msg);
+
+// Strings Methods
+
+// let name="   hello   ";
+// let res= name.trim();
+// console.log(name);
+// console.log(res);
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+// let name2="iLoveCoding";
+// let res2=name2.indexOf("C");
+// console.log(res2);
+// console.log(name2.repeat(2));
+
+// let name3="  Hello My name is SRK  ";
+// let res3=name3.toUpperCase().trim();
+// console.log(res3);
+// console.log(name3.slice(2,7));
+// console.log(name3.slice(7));
+// console.log(name3.slice(-5)); //name3.length-2 25-5=20;
+// console.log(name3.replace("SRK","Asif"));
+// console.log(name3.replace("H","X"));
+
+//Arrays Methods
+
+// let arr=[1,2,3,4];
+// console.log(arr);
+// console.log(arr[0]);
+
+// let arr2=["Apple","Banana","Grapes"];
+// console.log(arr2);
+// console.log(arr2[0][0]); // first one for index and second one for inside index;
+
+// console.log(arr2.push("orange")); //it will add the orange in last index
+// console.log(arr2.push("Lichi")); 
+// console.log(arr2.pop()); //it will delete the last index element
+// console.log(arr2.unshift("gauava")); // it will add the element in starting index
+// console.log(arr2.unshift("watermelon"));
+// console.log(arr2.shift("watermelon")); //it will delete the starting index of the element
+
+// let primary=["Apple","Banana","Grapes"];
+// let secondary=["lichi","Gauava","Water-Melon"];
+// console.log(primary.concat(secondary)); // it will add the array
+// console.log(primary.reverse()); // reverse the array
+// console.log(primary.slice(2,3)); //copies a portion of array;
+// console.log(secondary.slice(-2));
+// console.log(secondary.slice(secondary.length-1));
+// console.log(primary.slice(primary.length))
+
+
+// let cars=["xuv","maruti","audi","bmw"];
+// console.log(cars);
+// console.log(cars.splice(0,1));
+// console.log(cars.splice(0,0,"Bugati","ferrari"));
+
+// let chars=['a','d','b','c'];
+// console.log(chars.sort());
+// let num=[5,8,9,6,1,2]; // it will only sort character and strings elements
+// console.log(num.sort());
