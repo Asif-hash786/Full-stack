f703ad93102fcfcc4a7a23e468df681c160ceccf
Asif_hash786 <masif15881@gmail.com>
2025-10-31 00:56:45 +0530

    updated

---
+++ JS/Dom.js
@@ -0,0 +1,51 @@
+// let smallImage=document.getElementsByClassName("oldImg");
+
+// for(let i=0;i<smallImage.length;i++){
+//   smallImage[i].src="./asset2/spiderman-2591073_1280.jpg";
+//   console.log(`value of image no ${i} is changed`);
+// }
+
+// console.dir(document.querySelector("p"));  //only give one element
+// console.dir(document.querySelector(".oldImg"));
+// console.dir(document.querySelector("#description"));
+
+// console.log(document.querySelectorAll("div a"));  //give all element
+
+// let links=document.querySelectorAll(".box a");
+
+// for(links of links){
+//   links.style.color="yellow"; //inline style
+// }
+
+// for(let i=0;i<links.length;i++){
+//   links[i].style.color="pink";
+// }
+
+
+// let img=document.querySelector("img");
+// console.log(img.classList);
+
+//Adding class
+
+// let heading=document.querySelector("h1");
+// heading.classList.add("asif");
+// heading.classList.add("mop");
+// console.log(heading.classList);
+
+// // Removing class
+
+// heading.classList.remove("mop");
+// console.log(heading.classList);
+
+// // class exist or not;
+// console.log(heading.classList.contains("asif"));
+// console.log(heading.classList.contains("mop"));
+
+// // toggle between on and off    no--yes yes--no
+
+// console.log(heading.classList.toggle("asif"));
+// console.log(heading.classList.toggle("mop"));
+// console.log(heading.classList.toggle("asif"));
+
+
+
